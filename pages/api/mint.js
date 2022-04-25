eca8b84bb32d13a634c73edb1d8e8400b1c03575
Jonathan Gan <jonathan.gan@chain-bytes.com>
2022-04-25 10:53:26 -0400

    Going to test with mobile

--- pages/api/mint.js
+++ pages/api/mint.js
@@ -1,41 +1,48 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { ethers } from "ethers";
 
-export default async function mint(req, res){
+export default async function mint(req, res) {
+      // Address of the wallet you want to mint the NFT to, the NFT's metadata & Supply
+      const { mintToAddress, metadata, supply } = req.body
+      const delay = ms => new Promise(res => setTimeout(res, ms));
+      const metadataWithSupply = {
+        metadata,
+        supply,
+      }
+      console.log('NFT MINT INPUT: ', mintToAddress, metadata, supply)
 
-// Address of the wallet you want to mint the NFT to, the NFT's metadata & Supply
-  const { mintToAddress, metadata, supply } = req.body
-  const delay = ms => new Promise(res => setTimeout(res, ms));
-  const metadataWithSupply = {
-    metadata,
-    supply, 
-  }
+  switch (req.method) {
+    case "GET": {
+      return res.status(500).json({ message: "This API endpint is POST only" })
+    }
 
-  const sdk = new ThirdwebSDK(
-    new ethers.Wallet(
-      process.env.PRIVATE_KEY,
-      ethers.getDefaultProvider(process.env.NEXT_PUBLIC_RPC_URL),
-    )
-  );
+    case "POST": {
+      console.log('Will attempt to mint the NFT in 30s or less')
+      const sdk = new ThirdwebSDK(
+        new ethers.Wallet(
+          process.env.PRIVATE_KEY,
+          ethers.getDefaultProvider(process.env.NEXT_PUBLIC_RPC_URL),
+        )
+      );
 
-  const editionAddress = process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS;
-  const edition = sdk.getEdition(editionAddress);
-  console.log('NFT MINT INPUT: ', mintToAddress, metadata, supply)
-
-    while(delay(20000)) {
-      try {
-        const tx = await edition.mintTo(mintToAddress, metadataWithSupply);
-        res.status(200).json({ mint_NFT: tx })
-        const receipt = tx.receipt; // the transaction receipt
-        const tokenId = tx.id; // the id of the NFT minted
-        const nft = await tx.data(); // (optional) fetch details of minted NFT(nft);
-        if (nft) {
-          console.log('NFT MINT OUTPUT: ', receipt, tokenId, nft)
+      const editionAddress = process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS;
+      const edition = sdk.getEdition(editionAddress);
+      while (delay(30000)) {
+        try {
+          const tx = await edition.mintTo(mintToAddress, metadataWithSupply);
+          const receipt = tx.receipt; // the transaction receipt
+          const tokenId = tx.id; // the id of the NFT minted
+          // const nft = await tx.data(); // (optional) fetch details of minted NFT(nft);
+          if (await tx.data()) {
+            console.log('NFT MINT OUTPUT: ', receipt, tokenId, tx.data())
+            res.status(200).json({ mint_NFT: tx })
+            return 
+          }
+        } catch (error) {
+          console.log('Something happened, contract post may have timed out',error);
         }
-  
-      } catch (error) {
-        console.log(error);
+
       }
     }
-    console.log('Minting timed out ( < 20s )')
+  }
 }
