56df155b8e2411c6a8b944ab39d2383660ecaa54
Jonathan Gan <jonathan.gan@chain-bytes.com>
2022-04-25 10:28:45 -0400

    Added mint API timeout

--- pages/api/mint.js
+++ pages/api/mint.js
@@ -5,6 +5,7 @@ export default async function mint(req, res){
 
 // Address of the wallet you want to mint the NFT to, the NFT's metadata & Supply
   const { mintToAddress, metadata, supply } = req.body
+  const delay = ms => new Promise(res => setTimeout(res, ms));
   const metadataWithSupply = {
     metadata,
     supply, 
@@ -21,19 +22,20 @@ export default async function mint(req, res){
   const edition = sdk.getEdition(editionAddress);
   console.log('NFT MINT INPUT: ', mintToAddress, metadata, supply)
 
-    try {
-      const tx = await edition.mintTo(mintToAddress, metadataWithSupply);
-      const receipt = tx.receipt; // the transaction receipt
-      const tokenId = tx.id; // the id of the NFT minted
-      const nft = await tx.data(); // (optional) fetch details of minted NFT(nft);
-      console.log('NFT MINT OUTPUT: ', receipt, tokenId, nft)
-
-      if (nft) {
-        res.status(200).json({ mint_NFT: nft?.metadata })
+    while(delay(20000)) {
+      try {
+        const tx = await edition.mintTo(mintToAddress, metadataWithSupply);
+        res.status(200).json({ mint_NFT: tx })
+        const receipt = tx.receipt; // the transaction receipt
+        const tokenId = tx.id; // the id of the NFT minted
+        const nft = await tx.data(); // (optional) fetch details of minted NFT(nft);
+        if (nft) {
+          console.log('NFT MINT OUTPUT: ', receipt, tokenId, nft)
+        }
+  
+      } catch (error) {
+        console.log(error);
       }
-
-    } catch (error) {
-      console.log(error);
     }
-
+    console.log('Minting timed out ( < 20s )')
 }
