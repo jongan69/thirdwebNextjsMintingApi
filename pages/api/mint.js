61fc61eb82de3acc146e6caf66a06b1be7e2d95c
Jonathan Gan <jonathan.gan@chain-bytes.com>
2022-04-25 09:32:23 -0400

    Refactor:

--- pages/api/mint.js
+++ pages/api/mint.js
@@ -5,6 +5,10 @@ export default async function mint(req, res){
 
 // Address of the wallet you want to mint the NFT to, the NFT's metadata & Supply
   const { mintToAddress, metadata, supply } = req.body
+  const metadataWithSupply = {
+    metadata,
+    supply, 
+  }
 
   const sdk = new ThirdwebSDK(
     new ethers.Wallet(
@@ -12,28 +16,24 @@ export default async function mint(req, res){
       ethers.getDefaultProvider(process.env.NEXT_PUBLIC_RPC_URL),
     )
   );
+
   const editionAddress = process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS;
   const edition = sdk.getEdition(editionAddress);
-  console.log('DATA RECIECVED WAS: ', mintToAddress, metadata, supply)
+  console.log('NFT MINT INPUT: ', mintToAddress, metadata, supply)
 
     try {
-      const metadataWithSupply = {
-        metadata,
-        supply, 
-      }
-      
       const tx = await edition.mintTo(mintToAddress, metadataWithSupply);
       const receipt = tx.receipt; // the transaction receipt
       const tokenId = tx.id; // the id of the NFT minted
       const nft = await tx.data(); // (optional) fetch details of minted NFT(nft);
-      console.log('DATA OUTPUT: ', receipt, tokenId, nft)
+      console.log('NFT MINT OUTPUT: ', receipt, tokenId, nft)
 
       if (nft) {
         res.status(200).json({ mint_NFT: nft?.metadata })
       }
 
-
     } catch (error) {
       console.log(error);
     }
+
 }
